4bacbd75d9ae1bec4df388626acd943a9c2487d2
Jetro <jetro.karjalainen@gmail.com>
2023-05-03 12:20:24 +0300

    debugger

---
+++ Debugger/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Tuntilaskuri tunnit = new Tuntilaskuri();
+        tunnit.etene();
+        for (int i = 0; i < 22; i++) {
+            tunnit.etene();
+        }
+        tunnit.etene();
+        tunnit.etene();
+    }
+}
---
+++ Debugger/src/Tuntilaskuri.java
@@ -0,0 +1,15 @@
+public class Tuntilaskuri {
+    private int tunti;
+    public Tuntilaskuri() {
+        tunti = 0;
+    }
+    public void etene() {
+        tunti++;
+        if (tunti == 24) {
+            tunti = 0;
+        }
+    }
+    public int getTunti() {
+        return tunti;
+    }
+}
